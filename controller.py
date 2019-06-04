6cde6cff34883ebca081b6c36927b9b956d01ad9
matheusborn <matheusborn@hotmail.com>
2019-06-04 11:49:04 -0300

    alteracoes

--- controller.py
+++ controller.py
@@ -9,7 +9,7 @@ import json
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-PATH = './EventBus'
+PATH = './linux'
 
 repo = Repo(PATH)
 assert not repo.bare
@@ -19,24 +19,59 @@ commits = list(repo.iter_commits('HEAD'))
 def run():
 
     for index in range(len(commits)-1):
-        commit = commits[index]
         diff_index = commits[index+1].diff(commits[index].hexsha, create_patch=True,
                                            ignore_blank_lines=True, ignore_space_at_eol=True, diff_filter='cr')
+        count = 0
+        count2 = 0
         try:
             df = str(reduce(lambda x, y: str(x)+str(y), diff_index))
             df_lines = df.splitlines()
-            count = 0
             for line in df_lines:
-            # print(line)
                 try:
-                    if line[0] == '+' and line[1] != '-':
+                    if line[0] == '+':
                         count += 1
                     if line[0] == '-' and line[1] != '-':
                         count -= 1
                 except:
                     pass
+
             if count == 0:
-                print('foi ' + str(commit.hexsha))
+                for line in df_lines:
+                    if 'lhs:' in line:
+                        #print('count = ' + str(count))
+                        #print('count2 = ' + str(count2))
+
+                        if count > count2:
+                            print('aumentou complexidade')
+                            print(index)
+                        elif count2 > count:
+                            print('diminuiu complexidade')
+                            pass
+                        else:
+                            #print('manteve complexidade')
+                            pass
+                        count = 0
+                        count2 = 0
+                    try:
+                        if line[0] == '+' and '{' in line:
+                            count += 1
+                        elif line[0] == '-' and line[1] != '-' and '{' in line:
+                            count2 += 1
+                    except:
+                        pass
+
+                #print('count = ' + str(count))
+                #print('count2 = ' + str(count2))
+
+                if count > count2:
+                    print('aumentou complexidade')
+                    print(index)
+                elif count2 > count:
+                    #print('diminuiu complexidade')
+                    pass
+                else:
+                    #print('manteve complexidade')
+                    pass
             else:
                 pass
         except:
